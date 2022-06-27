04af2924f5bfd4107c13e58bcb86d8b7859d321d
devop44 <dev_op@bk.ru>
2022-06-27 13:36:30 +0300

    added Python Code as .py

--- checkpoint.py
+++ checkpoint.py
@@ -1,15 +1,15 @@
 def find_short_word(array):
     short_array = []
     array.append("invalided_stub_loop")
-    q = 0
-    for i in range(len(array)):
-        if 0 < q < 4:
-            short_array.append(array[i - 1])
-        q = 0
-        for k in array[i]:
-            q += 1
-            if q > 3:
-                q = 0
+    count_char = 0
+    for count_word in range(len(array)):
+        if 0 < count_char < 4:
+            short_array.append(array[count_word - 1])
+        count_char = 0
+        for loop_char in array[count_word]:
+            count_char += 1
+            if count_char > 3:
+                count_char = 0
                 break
     print(short_array)
 
